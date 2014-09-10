1e061335124aabfa87bf20b7a286e96fa2b41e8e
Kirill Zhukov <zhukov.kirill.96@gmail.com>
2014-09-10 11:16:12 -0700

    commit

--- app/src/main/java/com/minesweeper/kirill/minesweeperandroid/ImageCellAdapter.java
+++ app/src/main/java/com/minesweeper/kirill/minesweeperandroid/ImageCellAdapter.java
@@ -7,14 +7,15 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import static com.minesweeper.kirill.minesweeperandroid.Functionality.columns;
 import static com.minesweeper.kirill.minesweeperandroid.Functionality.generateBoard;
 
 /**
  * Created by kirill on 9/7/14.
  */
 public class ImageCellAdapter extends BaseAdapter {
-    private Context mContext;
-    private int[][] matrix;
+    public Context mContext;
+    public int[][] matrix;
 
     // Constructor
     public ImageCellAdapter(Context c) {
@@ -47,9 +48,42 @@ public class ImageCellAdapter extends BaseAdapter {
             imageView = (ImageView) convertView;
         }
 
-        System.out.println(position);
-        imageView.setImageResource(R.drawable.cell);
+        int x = getCoordinatesByPosition(position, columns)[0];
+        int y = getCoordinatesByPosition(position, columns)[1];
+
+        int value = matrix[x][y];
+
+        switch (value) {
+            case 0:
+                imageView.setImageResource(R.drawable.cell);
+            case 1:
+                imageView.setImageResource(R.drawable.num1);
+            case 2:
+                imageView.setImageResource(R.drawable.num2);
+            case 3:
+                imageView.setImageResource(R.drawable.num3);
+            case 4:
+                imageView.setImageResource(R.drawable.num4);
+            case 5:
+                imageView.setImageResource(R.drawable.num5);
+            case 6:
+                imageView.setImageResource(R.drawable.num6);
+            case 7:
+                imageView.setImageResource(R.drawable.num7);
+            case 8:
+                imageView.setImageResource(R.drawable.num8);
+            case 9:
+                imageView.setImageResource(R.drawable.bomb);
+        }
         return imageView;
+    }
+
+    public static int getPositionByCoordinates(int x, int y, int columns) {
+        return (x * columns + y);
+    }
+
+    public static int[] getCoordinatesByPosition(int position, int columns) {
+        return new int[]{position / columns, position % columns};
     }
 
 }
