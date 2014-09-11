934a4958a3fc2086a58df01ab2c9285e11007ba0
Kirill Zhukov <zhukov.kirill.96@gmail.com>
2014-09-11 00:34:55 -0700

    commit

--- app/src/main/java/com/minesweeper/kirill/minesweeperandroid/MainActivity.java
+++ app/src/main/java/com/minesweeper/kirill/minesweeperandroid/MainActivity.java
@@ -1,21 +1,27 @@
 package com.minesweeper.kirill.minesweeperandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 
-import static com.minesweeper.kirill.minesweeperandroid.Functionality.*;
+import static com.minesweeper.kirill.minesweeperandroid.Functionality.bombs;
+import static com.minesweeper.kirill.minesweeperandroid.Functionality.columns;
+import static com.minesweeper.kirill.minesweeperandroid.Functionality.generateBoard;
+import static com.minesweeper.kirill.minesweeperandroid.Functionality.printMatrix;
+import static com.minesweeper.kirill.minesweeperandroid.Functionality.rows;
 
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final int[][] matrix = generateBoard(8, 8, 10);
+        final int[][] matrix = generateBoard(rows, columns, bombs);
         printMatrix(matrix);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -23,48 +29,30 @@ public class MainActivity extends Activity {
         GridView fieldGridView = (GridView) findViewById(R.id.field_grid_view);
         fieldGridView.setAdapter(new ImageCellAdapter(this));
 
+
         fieldGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                int x = getCoordinatesByPosition(position, columns)[0];
-                int y = getCoordinatesByPosition(position, columns)[1];
-                int value = matrix[x][y];
-
-                ImageView imageView = (ImageView) view;
-                switch (value) {
-                    case 0:
-                        imageView.setImageResource(R.drawable.empty_cell);
-                        break;
-                    case 1:
-                        imageView.setImageResource(R.drawable.num1);
-                        break;
-                    case 2:
-                        imageView.setImageResource(R.drawable.num2);
-                        break;
-                    case 3:
-                        imageView.setImageResource(R.drawable.num3);
-                        break;
-                    case 4:
-                        imageView.setImageResource(R.drawable.num4);
-                        break;
-                    case 5:
-                        imageView.setImageResource(R.drawable.num5);
-                        break;
-                    case 6:
-                        imageView.setImageResource(R.drawable.num6);
-                        break;
-                    case 7:
-                        imageView.setImageResource(R.drawable.num7);
-                        break;
-                    case 8:
-                        imageView.setImageResource(R.drawable.num8);
-                        break;
-                    case 9:
-                        imageView.setImageResource(R.drawable.bomb);
-                        break;
-                }
+                openCell(view, matrix, position);
             }
         });
+
+        listenToRestartButton();
+    }
+
+    public void listenToRestartButton() {
+        Button button = (Button) findViewById(R.id.restartButton);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+
+        });
+
     }
 
     @Override
@@ -73,12 +61,100 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    public static int getPositionByCoordinates(int x, int y, int columns) {
-        return (x * columns + y);
-    }
+
+//    public static int getPositionByCoordinates(int x, int y, int columns) {
+//        return (x * columns + y);
+//    }
+
 
     public static int[] getCoordinatesByPosition(int position, int columns) {
         return new int[]{position / columns, position % columns};
     }
 
+
+    public void openCell(View view, int[][] matrix, int position) {
+        int x = getCoordinatesByPosition(position, columns)[0];
+        int y = getCoordinatesByPosition(position, columns)[1];
+        int value = matrix[x][y];
+
+        ImageView imageView = (ImageView) view;
+        switch (value) {
+            case 0:
+                imageView.setImageResource(R.drawable.empty_cell);
+                break;
+            case 1:
+                imageView.setImageResource(R.drawable.num1);
+                break;
+            case 2:
+                imageView.setImageResource(R.drawable.num2);
+                break;
+            case 3:
+                imageView.setImageResource(R.drawable.num3);
+                break;
+            case 4:
+                imageView.setImageResource(R.drawable.num4);
+                break;
+            case 5:
+                imageView.setImageResource(R.drawable.num5);
+                break;
+            case 6:
+                imageView.setImageResource(R.drawable.num6);
+                break;
+            case 7:
+                imageView.setImageResource(R.drawable.num7);
+                break;
+            case 8:
+                imageView.setImageResource(R.drawable.num8);
+                break;
+            case 9:
+                imageView.setImageResource(R.drawable.bomb);
+                imageView.setClickable(false);
+                //openAllCells(view, matrix);
+                break;
+        }
+    }
+
+
+    public void openAllCells(View view, int[][] matrix) {
+        int positions = rows * columns;
+        for (int position = 0; position < positions; position++) {
+            int x = getCoordinatesByPosition(position, columns)[0];
+            int y = getCoordinatesByPosition(position, columns)[1];
+            int value = matrix[x][y];
+
+            ImageView imageView = (ImageView) view;
+            switch (value) {
+                case 0:
+                    imageView.setImageResource(R.drawable.empty_cell);
+                    break;
+                case 1:
+                    imageView.setImageResource(R.drawable.num1);
+                    break;
+                case 2:
+                    imageView.setImageResource(R.drawable.num2);
+                    break;
+                case 3:
+                    imageView.setImageResource(R.drawable.num3);
+                    break;
+                case 4:
+                    imageView.setImageResource(R.drawable.num4);
+                    break;
+                case 5:
+                    imageView.setImageResource(R.drawable.num5);
+                    break;
+                case 6:
+                    imageView.setImageResource(R.drawable.num6);
+                    break;
+                case 7:
+                    imageView.setImageResource(R.drawable.num7);
+                    break;
+                case 8:
+                    imageView.setImageResource(R.drawable.num8);
+                    break;
+                case 9:
+                    imageView.setImageResource(R.drawable.bomb);
+                    break;
+            }
+        }
+    }
 }
